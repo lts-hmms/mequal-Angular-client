662aef1ce2e5c5b967e6342b8eed2c23684ac2c9
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-03-15 18:45:37 +0100

    feat: add genres dialog (not working)

--- src/app/genre-dialog/genre-dialog.component.ts
+++ src/app/genre-dialog/genre-dialog.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-genre-dialog',
   templateUrl: './genre-dialog.component.html',
-  styleUrls: ['./genre-dialog.component.scss']
+  styleUrls: ['./genre-dialog.component.scss'],
 })
 export class GenreDialogComponent {
-
+  genresArr: any[] = [];
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      genresArr: any;
+      Name: string;
+      Description: string;
+    }
+  ) {
+    console.log(data.genresArr);
+  }
 }
